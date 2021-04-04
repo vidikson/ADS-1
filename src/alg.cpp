3c5da5fadac0d5bff582ecbf9b05bd5915692d0f
vidikson <65093952+vidikson@users.noreply.github.com>
2021-04-04 15:54:37 +0300

    Кутузова Виктория

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,4 +3,27 @@
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
   return 0; // если ничего не найдено
+  int a = 0, b = size - 1, count = 0;
+    while (a < b){
+        int mid = (a + b) / 2;
+        if (arr[mid] < value){
+            a = mid+1;
+        }
+        else{
+             b = mid;
+        }
+    }
+    if (arr[a] == value){
+        while (arr[a] == value){
+            count++;
+            a++;
+        }
+    }
+    if (count){
+        return count;
+    }
+    else{
+        return 0;
+    }
+}
 }
