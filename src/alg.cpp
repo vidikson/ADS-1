cbe91a06331d52cb1549be801f9a2e76527487b0
vidikson <65093952+vidikson@users.noreply.github.com>
2021-04-04 16:13:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
-  int a = 0, b = size - 1, count = 0;
+  int a = 0;
+  int b = size - 1;
+  int count = 0;
     while (a < b) {
         int mid = (a + b) / 2;
         if (arr[mid] < value)
